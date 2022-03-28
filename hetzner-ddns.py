a55575543e1ede4ea3c2fa4beb08687dcbc26f7b
SpiritCroc <dev@spiritcroc.de>
2022-03-28 16:37:50 +0200

    Read multiple sections from the config

--- hetzner-ddns.py
+++ hetzner-ddns.py
@@ -37,8 +37,7 @@ import time
 args = docopt(__doc__)
 
 
-def merge_config_file():
-    global args
+def merge_config_file(args):
     basepath = os.path.dirname(os.path.abspath(__file__))
     candidate_config_files = [
         args["--config"],
@@ -46,6 +45,7 @@ def merge_config_file():
         "/etc/hetzner-ddns.conf",
     ]
 
+    all_args = []
     for filename in candidate_config_files:
         if filename is None:
             continue
@@ -54,22 +54,23 @@ def merge_config_file():
             config = configparser.ConfigParser()
             config.read(filename)
 
-            section_name = config.sections()[0]
-            print("    %-20s %s" % ("--zone", section_name))
-            args["--zone"] = section_name
+            for section_name in config.sections():
+                s_args = args.copy()
+                print("    %-20s %s" % ("--zone", section_name))
+                s_args["--zone"] = section_name
 
-            section = config[section_name]
-            for key in args.keys():
-                if key[2:] in section:
-                    value = section[key[2:]]
-                    print("    %-20s %s" % (key, value))
-                    args[key] = value
+                section = config[section_name]
+                for key in s_args.keys():
+                    if key[2:] in section:
+                        value = section[key[2:]]
+                        print("    %-20s %s" % (key, value))
+                        s_args[key] = value
+                all_args.append(s_args)
 
-    return args
+    return all_args
 
 
-def merge_defaults():
-    global args
+def merge_defaults(args):
     default_args = {
         "--ttl": 300,
         "--v4-api": "https://v4.ident.me/",
@@ -87,17 +88,24 @@ def merge_defaults():
             args[key] = value
 
 
-merge_config_file()
+if len(sys.argv) <= 1:
+    print("Using config file...")
+    all_args = merge_config_file(args)
+else:
+    print("Arguments passed, not using config file...")
+    all_args = args
 
-if args["--token"] is None:
-    print("API token must be provided")
-    sys.exit(1)
+for args in all_args:
 
-if args["--zone"] is None:
-    print("DNS zone must be provided")
-    sys.exit(1)
+    if args["--token"] is None:
+        print("API token must be provided")
+        sys.exit(1)
 
-merge_defaults()
+    if args["--zone"] is None:
+        print("DNS zone must be provided")
+        sys.exit(1)
+
+    merge_defaults(args)
 
 
 def get_addr(
@@ -185,66 +193,79 @@ def delete_record(rid):
     response.raise_for_status()
 
 
-def main():
-    kinds = []
-    delay = int(args["--repeat"])
+def main(all_args):
+    delay = 999999999999999999999999999999999999999
+    # Prepare
+    for args in all_args:
+        delay = min(int(args["--repeat"]), delay)
 
-    if not bool(args["--disable-v4"]):
-        kinds += ["A"]
+        print("Finding DNS zones for %s / %s..." % (args["--hostname"], args["--zone"]))
+        args["zone-internal"] = find_zone(args["--zone"])
 
-    if not bool(args["--disable-v6"]):
-        kinds += ["AAAA"]
-
-    print("Finding DNS zone...")
-    zone = find_zone(args["--zone"])
-
+    # Loop
     while True:
-        for kind in kinds:
-            if kind == "A":
-                print("Finding public IPv4 address...")
-                addr = get_addr(args["--v4-api"])
-                print("    %s" % addr)
-            else:
-                print("Finding public IPv6 address...")
-                addr = get_addr(args["--v6-api"])
-                print("    %s" % addr)
+        addr_a = None
+        addr_aaaa = None
+        for args in all_args:
+            print("Handling %s / %s" % (args["--hostname"], args["--zone"]))
+            kinds = []
+            if not bool(args["--disable-v4"]):
+                kinds += ["A"]
 
-            print("Finding existing %s record..." % kind)
-            rec = find_record(zone=zone, kind=kind, name=args["--hostname"])
+            if not bool(args["--disable-v6"]):
+                kinds += ["AAAA"]
 
-            if rec is not None:
-                if rec["value"] == addr:
-                    print("Existing record is up-to-date")
-                    continue
 
-                #print("Deleting existing %s record..." % kind)
-                #delete_record(rec["id"])
-                #print("    done")
-                print("Updating existing %s record..." % kind)
-                print(rec)
-                update_record(rec["id"],
-                    {
-                        "value": addr,
-                        "type": kind,
-                        "name": args["--hostname"],
-                        "zone_id": zone["id"],
-                        "ttl": args["--ttl"],
-                    }
-                )
-            else:
-                print("Creating new %s record..." % kind)
-                create_record(
-                    {
-                        "value": addr,
-                        "type": kind,
-                        "name": args["--hostname"],
-                        "zone_id": zone["id"],
-                        "ttl": args["--ttl"],
-                    }
-                )
-            print("    done")
+            for kind in kinds:
+                if kind == "A":
+                    if addr_a == None:
+                        print("Finding public IPv4 address...")
+                        addr_a = get_addr(args["--v4-api"])
+                        print("    %s" % addr_a)
+                    addr = addr_a
+                else:
+                    if addr_aaaa == None:
+                        print("Finding public IPv6 address...")
+                        addr_aaaa = get_addr(args["--v6-api"])
+                        print("    %s" % addr_aaaa)
+                    addr = addr_aaaa
+
+                print("Finding existing %s record..." % kind)
+                zone = args["zone-internal"]
+                rec = find_record(zone=zone, kind=kind, name=args["--hostname"])
+
+                if rec is not None:
+                    if rec["value"] == addr:
+                        print("Existing record is up-to-date")
+                        continue
+
+                    #print("Deleting existing %s record..." % kind)
+                    #delete_record(rec["id"])
+                    #print("    done")
+                    print("Updating existing %s record..." % kind)
+                    update_record(rec["id"],
+                        {
+                            "value": addr,
+                            "type": kind,
+                            "name": args["--hostname"],
+                            "zone_id": zone["id"],
+                            "ttl": args["--ttl"],
+                        }
+                    )
+                else:
+                    print("Creating new %s record..." % kind)
+                    create_record(
+                        {
+                            "value": addr,
+                            "type": kind,
+                            "name": args["--hostname"],
+                            "zone_id": zone["id"],
+                            "ttl": args["--ttl"],
+                        }
+                    )
+                print("    done")
 
         print(f"Sleeping for {delay} seconds...")
         time.sleep(delay)
 
-main()
+main(all_args)
